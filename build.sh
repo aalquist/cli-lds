46c4ecabbf1ddb09e6e66ed40f4f6ca76d7ee93d
Aaron Alquist <aaalquis@akamai.com>
2019-05-22 22:30:07 -0400

    Updating local build scripts

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 echo "building and testing code in python 3 image"
-#docker build --no-cache --tag cli-lds . && docker run --rm cli-lds python bin/akamai-lds
-docker build --tag cli-lds . && docker run --rm cli-lds python bin/akamai-lds 
-docker rmi cli-lds
 
-echo "testing python 2 not supported"
-namepostfix=$(date +%s | shasum | base64 | head -c 6)
-cat bin/akamai-lds | docker run --name testpy2$namepostfix -i --rm python:2.7.15-stretch 
-#docker rm testpy2$namepostfix
+commitId=$(git log -1 --pretty=%H)
+echo "$commitId for docker tag"
+NAME=akamai-lds
+DOCKERNAME="aaalquis/$NAME"
+echo $DOCKERNAME
+docker build -t $DOCKERNAME:$commitId .
+
+docker build --tag $NAME . 
+#docker run -v $(pwd):/cli-test --rm $NAME  python3 runtests.py
+#echo "docker run -v $(pwd):/cli-test --rm $NAME  /bin/bash"
+
+docker run -v $(pwd):/cli-test --rm $NAME python3 runtests.py
+docker run -v $(pwd):/cli-test --rm $NAME akamai lds help
+docker run -v $(pwd):/cli-test --rm $NAME akamai lds help list
+
+cat bin/$NAME | docker run --name testpy2$NAME -i --rm python:2.7.15-stretch 
+
+docker rmi $DOCKERNAME:$commitId
